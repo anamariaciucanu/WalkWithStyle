38988483d1dfbc035b0d3280d71f0139726ae3e6
Anamaria Ciucanu <anamaria.ciucanu@gmail.com>
2020-03-17 17:39:05 +0000

    Added skewed sine wave for Ankle
    The left foot now rotates, with the ankle rotating intuitively. More sync is needed between the YZ movement of the foot controller and the Ankle rotation. The next step would be to generalize this for multiple controllers and then focus on refinement. This work is currently inspired by the EMOTE model (Chi 2000).

--- Kate_02_ScriptFeet.py
+++ Kate_02_ScriptFeet.py
@@ -6,10 +6,12 @@ pi = math.pi
 animationStart = 0
 animationEnd = 100
 frameInterval = 20 #similar to speed
-footAmplitude = 20
+footAmplitude = 10
+ankleAmplitude = 5
+footSpeed = 10.0
 
 #Ellipse axes
-asq = 2.0
+asq = 3.0
 bsq = 1.0
 
 def resetFootTranslation(maxFrame):
@@ -17,11 +19,13 @@ def resetFootTranslation(maxFrame):
     cmds.setAttr('L_Foot_CTRL.translateX', 0)
     cmds.setAttr('L_Foot_CTRL.translateY', 0)
     cmds.setAttr('L_Foot_CTRL.translateZ', 0)
+    cmds.setAttr('L_Foot_CTRL.Ankle', 0)
     
    #Delete key frames
     cmds.cutKey( 'L_Foot_CTRL', time=(0,maxFrame), attribute='translateX', option='keys' )
     cmds.cutKey( 'L_Foot_CTRL', time=(0,maxFrame), attribute='translateY', option='keys' )
     cmds.cutKey( 'L_Foot_CTRL', time=(0,maxFrame), attribute='translateZ', option='keys' )
+    cmds.cutKey( 'L_Foot_CTRL', time=(0,maxFrame), attribute='Ankle', option='keys' )
     
 def createFootMotion():
     for i in range(animationStart, animationEnd, frameInterval):
@@ -31,17 +35,21 @@ def createFootMotion():
                 newX = 0  
                 newY = footAmplitude * math.sin(teta) / asq
                 newZ = footAmplitude * math.cos(teta) / bsq
+                newAnkle = ankleAmplitude * (-math.sin(teta) + math.sin(2*teta)/2.0)
                 
-                print(" sin(theta) ")
-                print(newY)
+                                 
+                print(" newAnkle ")
+                print(newAnkle)
                                       
                 oldX = cmds.getAttr('L_Foot_CTRL.translateX') 
                 oldY = cmds.getAttr('L_Foot_CTRL.translateY') 
                 oldZ = cmds.getAttr('L_Foot_CTRL.translateZ') 
+                oldAnkle = cmds.getAttr('L_Foot_CTRL.Ankle') 
                     
                 currentX = oldX 
-                currentY = oldY - newY
+                currentY = oldY + newY
                 currentZ = oldZ - newZ           
+                currentAnkle = newAnkle
     
                 cmds.setAttr('L_Foot_CTRL.translateX', currentX)
                 cmds.setKeyframe( 'L_Foot_CTRL', attribute='translateX', t=i+j )
@@ -49,6 +57,8 @@ def createFootMotion():
                 cmds.setKeyframe( 'L_Foot_CTRL', attribute='translateY', t=i+j )
                 cmds.setAttr('L_Foot_CTRL.translateZ', currentZ)
                 cmds.setKeyframe( 'L_Foot_CTRL', attribute='translateZ', t=i+j )   
+                cmds.setAttr('L_Foot_CTRL.Ankle', currentAnkle)
+                cmds.setKeyframe( 'L_Foot_CTRL', attribute='Ankle', t=i+j ) 
             else:
                 break
                 
